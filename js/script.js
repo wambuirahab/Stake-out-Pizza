953a9daf22fcf17227dcbbfa21a85a93a3be91f0
wambuirahab <sarahngaruiya2000@gmail.com>
2021-04-24 16:12:37 +0300

    Adding form function to submit the users input

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+$(document).ready(function() {
+  //form function to submit the users input, calculate total for the order, and prompt the user for some details
+    $("#text-center").submit(function(event) {
